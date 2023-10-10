5aeb37f03b43469d87a3a3943e0ade08e2e4b984
pranshushah <pranshu.shah23@gmail.com>
2023-10-10 11:10:15 +0530

    refactor: yup,zod validator fn returns function

--- src/zod.ts
+++ src/zod.ts
@@ -1,9 +1,10 @@
 import { ParseParams, TypeOf, ZodSchema } from 'zod';
 
 export async function validateZodSchema<TSchema extends ZodSchema>(
-	schema: TSchema,
-	options: Partial<ParseParams> = {},
-	data: TypeOf<TSchema>,
-): Promise<void> {
-	return schema.parse(data, { async: false, ...options });
+  schema: TSchema,
+  options: Partial<ParseParams> = {}
+) {
+  return (data: TypeOf<TSchema>) => {
+    return schema.parse(data, { async: false, ...options });
+  };
 }
