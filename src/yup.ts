993c21787c654b774f1e539fcf5b6cdb0b83c94d
pranshushah <pranshu.shah23@gmail.com>
2023-10-11 11:30:41 +0530

    refactor: only options as arg in validation func

--- src/yup.ts
+++ src/yup.ts
@@ -1,10 +1,9 @@
 import { Schema } from 'yup';
 
 export async function validateYupSchema<TSchema extends Schema>(
-  schema: TSchema,
   options: Parameters<TSchema['validate']>[1] = {}
 ) {
-  return (data: any) => {
+  return (schema: TSchema, data: any) => {
     return schema.validateSync(data, { abortEarly: false, ...options });
   };
 }
--- src/zod.ts
+++ src/zod.ts
@@ -1,10 +1,9 @@
 import { ParseParams, ZodSchema } from 'zod';
 
 export async function validateZodSchema<TSchema extends ZodSchema>(
-  schema: TSchema,
   options: Partial<ParseParams> = {}
 ) {
-  return (data: any) => {
+  return (schema: TSchema, data: any) => {
     return schema.parse(data, { async: false, ...options });
   };
 }
